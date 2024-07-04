6e55464ee4954124fd4b6d8ffa7315f17990ed08
the.sholaakanbi <oluwalonshola6@gmail.com>
2024-07-04 01:34:23 +0100

    Commit message describing your changes

--- PRODUCTS/routes/productRouter.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const  {
-    getAllProducts, updateProduct, deleteProduct, getProduct, createProduct
-} = require ("../controller/productController");
-
-router.route("/").get(getAllProducts).post(createProduct);
-router.route("/:productId").get(getProduct).patch(updateProduct).delete(deleteProduct);
-
-module.exports = router;
